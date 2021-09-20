7d1915501516c1a8810de1d8d9ceb3d3269f07e2
Ghalynho10 <mghalynho@gmail.com>
2021-09-20 17:01:10 -0400

    First implementation with a string as a key

---
+++ Lab1_Problem2/Lab1_Problem3/Lab1_Problem3/Lab1_Problem3/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  Lab1_Problem3
+//
+//  Created by Ghaly Nicolas Jules on 9/20/21.
+//
+
+
+
+
+
+#include <iostream>
+#include <utility>
+using namespace std;
+
+
+pair<double, double> ptC (0,0);
+pair<double, double> ptA;
+pair<double, double> ptB;
+
+
+
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
